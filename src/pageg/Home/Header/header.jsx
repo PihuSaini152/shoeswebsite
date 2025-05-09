832f1a16c00b1a67e97afef79e82f625640a1820
sachin <20develope23@gmail.com>
2025-05-09 12:11:22 +0530

    changes

--- src/pageg/Home/Header/header.jsx
+++ src/pageg/Home/Header/header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import './header.css/'
+import Head1 from '/headerimg/headimg1.jpg'
+import Head2 from '/headerimg/headimg2.jpg'
 
 function header() {
   return (
-    <div>
-      <h1>Hlwww</h1>
-    </div>
+    <>
+      <div className="headmain">
+        <div className="slidebar">
+          <div className="slider-track">
+
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </>
   )
 }
 
